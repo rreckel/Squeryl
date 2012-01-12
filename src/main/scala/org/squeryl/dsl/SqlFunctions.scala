32c1de30233cb6d21575e2c927968ac0d6eb4372
Roland RECKEL <rreckel@info-rreckel.ville.vdl.lu>
2012-01-12 17:00:05 +0100

    TRASAL-516 - Certificat de salaire 2011

--- src/main/scala/org/squeryl/dsl/SqlFunctions.scala
+++ src/main/scala/org/squeryl/dsl/SqlFunctions.scala
@@ -1,18 +1,6 @@
-/*******************************************************************************
- * Copyright 2010 Maxime Lévesque
- * 
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- * 
- *   http://www.apache.org/licenses/LICENSE-2.0
- * 
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- ***************************************************************************** */
+/*
+ * Copyright (c) 2012. Ville de Luxembourg. All rights reserved.
+ */
 package org.squeryl.dsl
 
 import ast._
@@ -27,7 +15,6 @@ trait SqlFunctions  {
   self: TypeArithmetic =>
 
   def max[A](e: NumericalExpression[A])      = new  UnaryAgregateLengthNeutralOp[A](e, "max")
-  def min[A](e: NumericalExpression[A])      = new  UnaryAgregateLengthNeutralOp[A](e, "min")
   def sum[A](e: NumericalExpression[A])      = new  UnaryAgregateLengthNeutralOp[A](e, "sum")
   def avg[A](e: NumericalExpression[A])      = new  UnaryAgregateFloatOp[A](e, "avg")
 
